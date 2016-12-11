026763522a088c020c06fcc11b1f93916b4c8e2b
Alasdair Smith <ali@alasdairsmith.co.uk>
2016-12-11 17:58:09 +0000

    Move source file to src directory

--- test/index.test.js
+++ test/index.test.js
@@ -2,7 +2,7 @@ import fs from 'fs'
 import path from 'path'
 import webpack from 'webpack'
 
-import SentryWebpackPlugin from '../index'
+import SentryWebpackPlugin from '../src/index'
 
 import {
 	cleanUpRelease,
