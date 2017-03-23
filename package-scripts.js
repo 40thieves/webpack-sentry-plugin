7709c16fe3b04c6530593387f252901f6d54cf00
Alasdair Smith <ali@alasdairsmith.co.uk>
2017-03-23 21:11:35 +0000

    Configure package scripts & add descriptons

---
+++ package-scripts.js
@@ -0,0 +1,22 @@
+module.exports = {
+  scripts: {
+    build: {
+      description: 'Run babel to compile files',
+      script: 'babel src --out-dir dist'
+    },
+    prepublish: {
+      description: 'Validate and build before release',
+      script: 'nps validate && nps build'
+    },
+    validate: {
+      description: 'Runs code formatting, linting and tests',
+      script: 'nps format lint test'
+    },
+    format: {
+      description: 'Format code with prettier-eslint',
+      script: 'prettier-eslint --write "{src,test}/**/*.js"'
+    },
+    lint: 'eslint src test; exit 0',
+    test: 'jest'
+  }
+};
